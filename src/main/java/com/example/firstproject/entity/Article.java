19318ce96dd3c60fbbb4dea9d20f2ea260c1307e
yujin <yujin00530@naver.com>
2023-08-20 01:10:43 +0900

    commit

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,5 +1,20 @@
 package com.example.firstproject.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Article {
+	@Id       // 엔티티의 대표값 지정
+	@GeneratedValue	// 자동 생성 기능 추가(숫자가 자동으로 매겨짐)
+	private Long id;  
+	@Column  // title 필드 선연, DB테이블의 title열과 연결
+	private String title; 
+	@Column	 // content필드 선언 
+	
+	private String content;
+
 
 }
